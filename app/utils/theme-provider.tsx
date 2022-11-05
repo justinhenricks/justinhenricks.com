fbf4a17b3a6bc5c7dea62b15667822a73f1074d6
Justin Henricks <justinhenricks@gmail.com>
2022-11-05 16:29:46 -0400

    OK Put cypress back in! Found the theme bug

--- app/utils/theme-provider.tsx
+++ app/utils/theme-provider.tsx
@@ -93,7 +93,19 @@ function useTheme() {
 
 const clientThemeCode = `
 ;(() => {
-  // ...
+  const theme = window.matchMedia(${JSON.stringify(prefersDarkMQ)}).matches
+    ? 'dark'
+    : 'light';
+  const cl = document.documentElement.classList;
+  const themeAlreadyApplied = cl.contains('light') || cl.contains('dark');
+  if (themeAlreadyApplied) {
+    // this script shouldn't exist if the theme is already applied!
+    console.warn(
+      "Hi there, could you let Matt know you're seeing this message? Thanks!",
+    );
+  } else {
+    cl.add(theme);
+  }
 
   const meta = document.querySelector('meta[name=color-scheme]');
   if (meta) {
