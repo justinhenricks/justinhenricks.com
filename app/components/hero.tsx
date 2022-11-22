e63796c493cd062b87b65813c04c33f3ff36ac64
Justin Henricks <justinhenricks@gmail.com>
2022-11-21 21:31:26 -0500

    installed fonts, started working on style guide
    TODO add the rest of base classes to css

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -5,8 +5,7 @@ export default function Hero() {
     <div className="flex flex-col items-center justify-center gap-12 md:w-full md:flex-row md:justify-around">
       <img className="max-w-xs rounded-full md:order-2" src={justin} />
       <div className="flex flex-col justify-center">
-        <div className="text-5xl">Hiiiiii</div>
-        <div className="text-2xl">These are the times</div>
+        <h1>These are the times</h1>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 function withOpacityValue(variable) {
   return ({ opacityValue }) => {
+    console.log("opacity");
+    console.log(opacityValue);
     if (opacityValue === undefined) {
       return `rgb(var(${variable}))`;
     }
@@ -14,16 +16,32 @@ module.exports = {
   darkMode: "class",
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Noto Sans", ...defaultTheme.fontFamily.sans],
-      },
-      colors: {
-        light: withOpacityValue("--color-light"),
-        dark: withOpacityValue("--color-dark"),
-        accent: withOpacityValue("--color-accent"),
-      },
+    colors: {
+      light: "#FDFDFD",
+      dark: "#121212",
+      blackberry: "#000022",
     },
+    boxShadow: {
+      sm: "2px 2px 0px #000022",
+      md: "3px 3px 0px #000022",
+      lg: "0px -7px 4px rgba(0, 0, 0, 0.5)",
+    },
+    fontSize: {
+      xxs: ["12px", { lineHeight: "12px" }],
+      xs: ["12px", { lineHeight: "18px" }],
+      s: ["14px", { lineHeight: "21.6px" }],
+      m: ["16px", { lineHeight: "24px" }],
+      xl: ["18px", { lineHeight: "22.32px" }],
+      "2xl": ["20px", { lineHeight: "24px" }],
+      "3xl": ["22px", { lineHeight: "28.16px" }],
+      "4xl": ["24px", { lineHeight: "31.68px" }],
+      "5xl": ["28px", { lineHeight: "35.84px" }],
+    },
+    fontFamily: {
+      satoshi: "Satoshi, sans-serif",
+      roboto: "Roboto, sans-serif",
+    },
+    extend: {},
   },
   plugins: [],
 };
